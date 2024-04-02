578642cc01e4f9f684055b49ed4049750f51dfeb
Parth Sarathi Dixit <110755500+parth-psd-009@users.noreply.github.com>
2024-04-02 07:51:06 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const connectDB = async () => {
     try {
         await mongoose.connect(
-            // `mongodb+srv://psd:psd@cluster0.gvygig3.mongodb.net/?retryWrites=true&w=majority`,
             process.env.MONGO_URL,
             {
                 useNewUrlParser: true,
